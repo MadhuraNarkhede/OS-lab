32d25df6b2bb02d89ad910231e10c70e10f63b05
Madhura Lahu  Narkhede <156207107+MadhuraNarkhede@users.noreply.github.com>
2024-11-09 09:31:04 +0530

    Add files via upload

---
+++ fork_and_wait.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+void bubble_sort(int arr[20],int n)
+{
+ int i,j,temp;
+ 
+ for( i=0;i<n;i++)
+ {
+  for(j=0;j<n;j++)
+  {
+   if(arr[j]>arr[j+1])
+   {
+    temp=arr[j];
+    arr[j]=arr[j+1];
+    arr[j+1]=arr[j];
+    }
+  }
+}
+}
+
+void fork1()
+{
+ int arr[20],n;
+ 
+ printf("\n Enter the number of elements in array(max 20) : ");
+ scanf("%d",&n);
+ printf("\n Enter the elements : ");
+ for(int i=0;i<n;i++)
+ {
+  scanf("%d",&arr[i]);
+  }
+  
+  int pid=fork();
+   
+   if(pid==0)
+   {
+    sleep(5);
+    printf("\n child process ");
+    printf("\n child process id=%d\n",getpid());
+    
+    bubble_sort(arr,n);
+    
+    printf("Elements sorted by child process by :");
+    for(int i=0;i<n;i++)
+ {
+  printf("%d",arr[i]);
+  printf("\b");printf("\n parent process ");                   
+    printf("\n parent process id=%d\n",getpid());      
+  }
+  printf("\n parent process ");
+    printf("\n parent process id=%d\n",getpid());
+    system("ps -x");                 
+  
+  printf("\n");     
+  }
+ else
+ {
+ printf("\n parent process ");
+    printf("\n parent process id=%d\n",getpid());
+    
+    bubble_sort(arr,n);
+    
+    printf("Elements sorted by parent process by :");
+    for(int i=0;i<n;i++)
+ {
+  printf("%d",arr[i]);
+  printf("\b");
+  }
+  
+  printf("\n");
+  }
+  }
+  
+  int main()
+  {
+   fork1();
+   return 0;
+   }
+  
