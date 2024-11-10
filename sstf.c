dc1d479d89894dad54db3797c87fee2dbeb0a6f8
Madhura Lahu  Narkhede <156207107+MadhuraNarkhede@users.noreply.github.com>
2024-11-10 11:24:44 +0530

    Update sstf.c

--- sstf.c
+++ sstf.c
@@ -1,59 +1,68 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
-void sstf(int req[], int n, int head)
-{
-int total_seek = 0, min, diff, idx, i, j;
-int visited[100] = {0};
-printf("SSTF Seek Sequence: ");
-for (i = 0; i < n; i++)
-{
-min = 1000;
-idx = -1;
-for (j = 0; j < n; j++)
-{
-if (!visited[j])
-{
-diff = abs(req[j] - head);
-if (diff < min)
-{
-min = diff;
-idx = j;
+int cmpfunc(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
 }
-}
-}
-visited[idx] = 1;
-total_seek += abs(req[idx] - head);
-head = req[idx];
-printf("%d ", head);
-}
-printf("\nTotal Seek Time (SSTF): %d\n", total_seek);
-}
-int cmpfunc(const void *a, const void *b)
-{
-return (*(int *)a - *(int *)b);
+
+void c_look(int req[], int n, int head) {
+    int total_seek = 0, i;
+
+    int left[100], right[100], l_count = 0, r_count = 0;
+
+    for (i = 0; i < n; i++) {
+        if (req[i] < head) {
+            left[l_count++] = req[i];
+        } else if (req[i] > head) {
+            right[r_count++] = req[i];
+        }
+    }
+
+    qsort(left, l_count, sizeof(int), cmpfunc);
+    qsort(right, r_count, sizeof(int), cmpfunc);
+
+    printf("\nC-LOOK sequence is as follows: \n");
+    printf("%d ", head);
+
+    // Process right requests
+    for (i = 0; i < r_count; i++) {
+        total_seek += abs(head - right[i]);
+        head = right[i];
+        printf("%d ", head);
+    }
+
+    // Jump to the first left request
+    if (l_count > 0) {
+        total_seek += abs(head - left[0]);
+        head = left[0];
+        printf("%d ", head);
+    }
+
+    // Process left requests
+    for (i = 0; i < l_count; i++) {
+        total_seek += abs(head - left[i]);
+        head = left[i];
+        printf("%d ", head);
+    }
+
+    printf("\nTotal Seek Time is: %d\n", total_seek);
 }
 
 int main() {
-int n, i, head, disk_size, direction;
-int req[100];
-printf("Enter number of requests: ");
-scanf("%d", &n);
-printf("Enter request sequence: ");
-for (i = 0; i < n; i++)
-{
-scanf("%d", &req[i]);
-}
-printf("Enter initial head position: ");
-scanf("%d", &head);
-printf("Enter disk size: ");
-scanf("%d", &disk_size);
-printf("Enter direction (1 for up, -1 for down): ");
-scanf("%d", &direction);
-sstf(req, n, head);
-scan(req, n, head, disk_size, direction);
-clook(req, n, head);
-return 0;
-}
+    int n, i, req[100], head;
 
+    printf("\nEnter the number of requests: ");
+    scanf("%d", &n);
+
+    printf("\nEnter the sequence: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &req[i]);
+    }
+
+    printf("\nEnter the initial position of head: ");
+    scanf("%d", &head);
+
+    c_look(req, n, head);
+
+    return 0;
+}
