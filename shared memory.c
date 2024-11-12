d56fcad11a75b0c6192689a5ebfa238e035f5641
Madhura Lahu  Narkhede <156207107+MadhuraNarkhede@users.noreply.github.com>
2024-11-13 04:52:35 +0530

    Update shared memory.c

--- shared memory.c
+++ shared memory.c
@@ -1,60 +1,65 @@
-Practical no.11
-//server.c
+//Server
 #include <stdio.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdlib.h>
-struct data
-{
-char str[100];
-int flag;
+
+struct data {
+    char str[100];
+    int flag;
 };
-void main()
-{
-key_t key = ftok("abc.txt",'A');
-int shmid = shmget(key, 1024, 0777|IPC_CREAT);
-struct data*d = shmat(shmid, NULL, 0);
-d->flag = 0;
-printf("\n Enter data: ");
-gets(d->str);
-d->flag=1;
-while(d->flag!= -1)
-{
-printf("\n waiting \n ");
-sleep(2);
+
+void main() {
+    key_t key = ftok("abc.txt", 'A');
+    int shmid = shmget(key, 1024, 0777 | IPC_CREAT);
+    struct data *d = shmat(shmid, NULL, 0);
+    d->flag = 0;
+
+    printf("\n Enter data: ");
+    fgets(d->str, sizeof(d->str), stdin); // Missing semicolon fixed
+
+    d->flag = 1;
+    while (d->flag != -1) {
+        printf("\n waiting \n");
+        sleep(2);
+    }
+
+    shmdt(d);
 }
-shmdt(d);
-}
-//client.c
+
+//client
 #include <stdio.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdlib.h>
-struct data
-{
-char str[100];
-int flag;
+
+struct data {
+    char str[100];
+    int flag;
 };
-void main()
-{
-key_t key = ftok("abc.txt",'A');
-int shmid = shmget(key, 1024, 0777|IPC_CREAT);
-struct data*d = shmat(shmid, NULL, 0);
-d->flag = 0;
-while(d->flag!= 1)
-{
-printf("\n waiting \n ");
-sleep(2);
-}
-printf("\n Accepted data is: ");
-printf("%s", d->str);
-d->flag=-1;
-printf("\n");
-shmdt(d);
-shmctl(shmid, IPC_RMID, NULL);
+
+void main() {
+    key_t key = ftok("abc.txt", 'A');
+    int shmid = shmget(key, 1024, 0777 | IPC_CREAT);
+    struct data *d = shmat(shmid, NULL, 0);
+    d->flag = 0;
+
+    while (d->flag != 1) {
+        printf("\n waiting \n");
+        sleep(2);
+    }
+
+    printf("\n Accepted data is: ");
+    printf("%s", d->str);
+
+    d->flag = -1;
+    printf("\n");
+
+    shmdt(d);
+    shmctl(shmid, IPC_RMID, NULL);
 }
 
